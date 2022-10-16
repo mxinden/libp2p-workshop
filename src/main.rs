add6b14409c494fd9edc4f840d364a909b3d9186
Max Inden <mail@max-inden.de>
2022-10-16 20:51:57 +0100

    *: Add scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use clap::Parser;
+use std::error::Error;
+
+#[derive(Debug, Parser)]
+#[clap(name = "libp2p-workshop-node")]
+struct Opts {}
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+    let _opts = Opts::parse();
+
     println!("Hello, world!");
+
+    Ok(())
 }
