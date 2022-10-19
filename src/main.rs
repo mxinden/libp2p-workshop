7c1d812ed0005ce1ff7eaa0ac9081a02bd6f409c
Elena Frank <elena.frank@protonmail.com>
2022-10-19 02:35:37 +0200

    Add event loop

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
-use prost::Message;
 mod codec;
+mod event_loop;
+
 use async_std::io;
-use asynchronous_codec::{Decoder, Encoder};
+
 use clap::Parser;
-use futures::{prelude::*, select, stream::StreamExt};
-use futures_timer::Delay;
+use futures::{
+    channel::{mpsc, oneshot},
+    prelude::*,
+    select,
+    stream::StreamExt,
+};
+
 use libp2p::{
     core, dns,
-    gossipsub::{self, GossipsubEvent, GossipsubMessage},
+    gossipsub::{self},
     identify, identity,
     multiaddr::Protocol,
     noise, relay,
-    request_response::{self, RequestResponseEvent, RequestResponseMessage},
-    swarm::SwarmEvent,
+    request_response::{self},
     tcp, yamux, Multiaddr, NetworkBehaviour, PeerId, Swarm, Transport,
 };
-use std::{
-    collections::{hash_map::Entry, HashMap},
-    error::Error,
-    io::Cursor,
-    iter,
-    os::unix::prelude::FileExt,
-    time::Duration,
-};
+
+use event_loop::{Command, Event, EventLoop};
+use std::{error::Error, iter, os::unix::prelude::FileExt, time::Duration};
 
 #[allow(clippy::derive_partial_eq_without_eq)]
 mod message_proto {
@@ -41,76 +41,42 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // # Joining the network
     // ----------------------------------------
 
-    // Listen on a new address so that other peers can dial us.
-    //
-    // - IP 0.0.0.0 lets us listen on all network interfaces.
-    // - Port 0 uses a port assigned by the OS.
-    let local_address = "/ip4/0.0.0.0/tcp/0".parse().unwrap();
-    network.listen_on(local_address)?;
+    // // Listen on a new address so that other peers can dial us.
+    // //
+    // // - IP 0.0.0.0 lets us listen on all network interfaces.
+    // // - Port 0 uses a port assigned by the OS.
+    // let local_address = "/ip4/0.0.0.0/tcp/0".parse().unwrap();
+    // network.listen_on(local_address)?;
+
+    // // Dial the bootstrap node.
+    // network.dial(opts.bootstrap_node)?;
 
     network.listen_on(opts.bootstrap_node.clone().with(Protocol::P2pCircuit))?;
-
-    // Dial the bootstrap node.
-    network.dial(opts.bootstrap_node)?;
 
     // ----------------------------------------
     // Send and receive messages in the network.
     // ----------------------------------------
     let chat_topic = gossipsub::IdentTopic::new("chat");
     let addrs_topic = gossipsub::IdentTopic::new("addresses");
-    let provider_topic = gossipsub::IdentTopic::new("files");
+    let files_topic = gossipsub::IdentTopic::new("files");
 
     network.behaviour_mut().gossipsub.subscribe(&chat_topic)?;
     network.behaviour_mut().gossipsub.subscribe(&addrs_topic)?;
-    network
-        .behaviour_mut()
-        .gossipsub
-        .subscribe(&provider_topic)?;
+    network.behaviour_mut().gossipsub.subscribe(&files_topic)?;
 
     // Read full lines from stdin
     let mut stdin = io::BufReader::new(io::stdin()).lines().fuse();
-
-    let mut file_list = HashMap::new();
-    let mut providing = HashMap::<String, String>::new();
-    let mut pending_requests = HashMap::new();
 
     // ----------------------------------------
     // Run the network until we established a connection to the bootstrap node
     // and exchanged identify into
     // ----------------------------------------
 
-    let mut delay = Delay::new(Duration::from_secs(5)).fuse();
+    let (mut client, mut events_receiver) =
+        Client::new(network, files_topic, chat_topic, addrs_topic);
 
     loop {
         select! {
-            _ = delay => {
-                for filename in providing.keys() {
-                    let listen_addrs = network.listeners().map(|a| a.to_vec()).collect();
-
-                    let announcement = message_proto::FileAnnouncement {
-                        filename: filename.clone(),
-                        addrs: listen_addrs,
-                    };
-
-                    let mut encoded_msg = bytes::BytesMut::new();
-                    announcement.encode(&mut encoded_msg)?;
-                    let mut dst = bytes::BytesMut::new();
-                    unsigned_varint::codec::UviBytes::default().encode(encoded_msg.freeze(), &mut dst)?;
-
-                    match network
-                        .behaviour_mut()
-                        .gossipsub
-                        .publish(provider_topic.clone(), dst)
-                    {
-                        Ok(_) => {
-                            log::info!("Published file {:?}", filename);
-                        },
-                        Err(e) => log::warn!("Publish error: {:?}", e),
-                    }
-                }
-                delay = Delay::new(Duration::from_secs(5)).fuse();
-            },
-
             // Parse lines from Stdin
             line = stdin.select_next_some() => {
 
@@ -124,139 +90,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     }
                 };
                 match prefix {
-                    "MSG" => {
-                        if let Err(e) = network
-                            .behaviour_mut()
-                            .gossipsub
-                            .publish(chat_topic.clone(), arg.as_bytes())
-                        {
-                            log::info!("Publish error: {:?}", e);
-                        }
+                    "MSG" => match client.send_message(arg.to_string()).await {
+                        Ok(()) => {}
+                        Err(e) => log::info!("Publish error: {:?}", e),
                     }
                     "GET" => {
-                        let provider_id = match file_list.get(&arg.to_string()) {
-                            Some(provider_id) => provider_id,
-                            None => {
-                                log::info!("No provider known for: {:?}", arg);
-                                continue;
+                        let file_name = arg.to_string();
+                        let data = match client.request_file(file_name.clone()).await {
+                            Ok(data) => data,
+                            Err(err) => {
+                                log::warn!("Error getting file {}", err);
+                                continue
                             }
                         };
-                        let request_id = network.behaviour_mut().request_response.send_request(provider_id, arg.as_bytes().to_vec());
-                        pending_requests.insert(request_id, arg.to_string());
-                        log::info!("Requested file for: {:?}", arg);
-                    }
-                    "PUT" => {
-                        let path = std::path::Path::new(arg);
-                        if let Err(err) = std::fs::File::open(&path) {
-                            log::info!("Can not access file {:?}: {:?}", arg, err);
-                            continue;
-                        }
-                        let filename = path.file_name().and_then(|s| s.to_str()).map(|s| s.to_owned()).unwrap();
-                        providing.insert(filename, arg.to_string());
-                    }
-                    other => {
-                        log::info!("Invalid prefix: Expected MSG|GET|PUT, found {}", other)
-                    }
-                }
-            },
-
-
-            // Wait for an event happening on the network.
-            // The `match` statement allows to match on the type
-            // of event an handle each event differently.
-            event = network.select_next_some() => match event {
-
-                // Case 1: We are now actively listening on an address
-                SwarmEvent::NewListenAddr { address, .. } => {
-                    log::info!("Listening on {}.", address);
-
-                    if let Err(e) = network
-                        .behaviour_mut()
-                        .gossipsub
-                        .publish(addrs_topic.clone(), address.to_vec())
-                    {
-                        log::debug!("Publish error: {:?}", e);
-                    }
-                }
-
-                // Case 2: A connection to another peer was established
-                SwarmEvent::ConnectionEstablished { endpoint, .. } => {
-                    log::info!("Connected to {}.", endpoint.get_remote_address());
-                }
-
-                // Case 2: A connection to another peer was established
-                SwarmEvent::ConnectionClosed { endpoint, .. } => {
-                    log::debug!("Connection closed to {}.", endpoint.get_remote_address());
-                }
-
-                // Case 3: A remote send us their identify info with the identify protocol.
-                SwarmEvent::Behaviour(BehaviourEvent::Identify(identify::Event::Received {
-                    peer_id: _,
-                    info: identify::Info { agent_version, .. },
-                })) => {
-                    log::info!("Agent version {}", agent_version);
-                }
-
-                // Case 4: We received a message from another peer.
-                SwarmEvent::Behaviour(BehaviourEvent::Gossipsub(GossipsubEvent::Message {
-                    message_id,
-                    message: GossipsubMessage { topic, data, source, ..},
-                    ..
-                })) => {
-                    let source = source.unwrap();
-                    if topic == chat_topic.hash() {
-                        log::info!(
-                            "Got message\n\tMessage Id: {}\n\tSender: {:?}\n\tMessage: {:?}",
-                            message_id,
-                            source,
-                            String::from_utf8_lossy(&data),
-                        );
-                    } else if topic == provider_topic.hash(){
-                        let mut b: bytes::BytesMut = data.as_slice().into();
-                        let mut uvi: unsigned_varint::codec::UviBytes  = unsigned_varint::codec::UviBytes::default();
-                        let file_announcement =match
-                         uvi.decode(&mut b)?
-                            .and_then(|msg| message_proto::FileAnnouncement::decode(Cursor::new(msg)).ok()) {
-                                Some(decoded) => decoded,
-                                None => {
-                                    log::debug!("Received invalid message: {:?}", data);
-                                    continue;
-                                }
-                            };
-                        for addr in file_announcement.addrs {
-                            network.behaviour_mut().request_response.add_address(&source, Multiaddr::try_from(addr)?);
-                        }
-                        if let Entry::Vacant(e)= file_list.entry(file_announcement.filename.clone()) {
-                            e.insert(source);
-                            log::info!("{:?} is now providing file {:?}", source,file_announcement.filename );
-                        }
-                    } else if topic == addrs_topic.hash() {
-                        let addr = Multiaddr::try_from(data).unwrap();
-                        network.behaviour_mut().request_response.add_address(&source, addr)
-                    }
-                }
-
-                SwarmEvent::Behaviour(BehaviourEvent::RequestResponse(
-                    RequestResponseEvent::Message { message, .. },
-                )) => match message {
-                    RequestResponseMessage::Request {
-                        request, channel, ..
-                    } => {
-                        let file_content = match String::from_utf8(request.clone()).ok().and_then(|file_name| providing.get(&file_name))
-                        .and_then(|file_path|std::fs::read(&file_path).ok()) {
-                            Some(path) => path,
-                            None => {
-                                log::info!("Got request for invalid file path: {:?}", request);
-                                continue;
-                            }
-                        };
-                        let _ = network.behaviour_mut().request_response.send_response(channel, file_content);
-                    }
-                    RequestResponseMessage::Response {
-                        request_id,
-                        response,
-                    } => {
-                        let file_name = pending_requests.remove(&request_id).unwrap();
                         let file = match std::fs::File::create(file_name.clone()) {
                             Ok(file) => file,
                             Err(err) => {
@@ -264,16 +110,59 @@ async fn main() -> Result<(), Box<dyn Error>> {
                                 continue
                             }
                         };
-                        match file.write_all_at(&response, 0) {
+                        match file.write_all_at(&data, 0) {
                             Ok(()) => log::info!("Downloaded new file: {:?}", file_name),
                             Err(err) => {
                                 log::warn!("Error write to file at {}: {:?}", file_name, err)
                             }
                         }
                     }
-                },
+                    "PUT" => {
+                        let file_name = arg.to_string();
+                        match client.start_providing(file_name.clone()).await {
+                            Ok(()) => log::info!("Published {:?}", file_name),
+                            Err(e) => log::warn!("Publishing file {} failed {}", file_name, e),
+                        }
+                    }
+                    other => {
+                        log::info!("Invalid prefix: Expected MSG|GET|PUT, found {}", other)
+                    }
+                }
+            },
 
-                event => log::debug!("{:?}", event),
+            // Wait for an event happening on the network.
+            // The `match` statement allows to match on the type
+            // of event an handle each event differently.
+            event = events_receiver.select_next_some() => match event {
+                // Case 1: We are now actively listening on an address
+                Event::NewListenAddr { addr } => {
+                    log::info!("Listening on {}.", addr);
+                }
+
+                // Case 2: A connection to another peer was established
+                Event::ConnectionEstablished { endpoint } => {
+                    log::info!("Connected to {}.", endpoint.get_remote_address());
+                }
+
+                // Case 3: A remote send us their identify info with the identify protocol.
+                Event::Identify( identify::Info { agent_version, .. }) => {
+                    log::info!("Agent version {}", agent_version);
+                }
+
+                // Case 4: We learned about a file that another peer is providing.
+                Event::NewProvider { peer, file} => {
+                    log::info!("{:?} is now providing file {:?}", peer, file );
+                }
+
+                // Case 5: A remote peer published a message to the network
+                Event::NewMessage {peer, message_id, message} => {
+                    log::info!(
+                        "Got message\n\tMessage Id: {}\n\tSender: {:?}\n\tMessage: {:?}",
+                        message_id,
+                        peer,
+                        String::from_utf8_lossy(&message),
+                    );
+                }
             }
         }
     }
@@ -337,11 +226,16 @@ async fn create_network() -> Result<Swarm<Behaviour>, Box<dyn Error>> {
     config.set_request_timeout(Duration::from_secs(60));
 
     // Enable direct 1:1 request-response messages.
-    let direct_message_protocol = request_response::RequestResponse::new(
-        codec::Codec,
-        iter::once((codec::Protocol, request_response::ProtocolSupport::Full)),
-        config,
-    );
+    let direct_message_protocol = {
+        let mut config = request_response::RequestResponseConfig::default();
+        config.set_connection_keep_alive(Duration::from_secs(60));
+        config.set_request_timeout(Duration::from_secs(60));
+        request_response::RequestResponse::new(
+            codec::Codec,
+            iter::once((codec::Protocol, request_response::ProtocolSupport::Full)),
+            config,
+        )
+    };
 
     // ----------------------------------------
     // # Create our transport layer
@@ -378,8 +272,85 @@ async fn create_network() -> Result<Swarm<Behaviour>, Box<dyn Error>> {
     ))
 }
 
+#[derive(Clone)]
+pub struct Client {
+    sender: mpsc::Sender<Command>,
+}
+
+impl Client {
+    pub fn new(
+        network: Swarm<Behaviour>,
+        files_topic: gossipsub::IdentTopic,
+        chat_topic: gossipsub::IdentTopic,
+        address_topic: gossipsub::IdentTopic,
+    ) -> (Self, mpsc::Receiver<Event>) {
+        let (event_tx, event_rx) = mpsc::channel(10);
+        let (command_tx, command_rx) = mpsc::channel(10);
+        async_std::task::spawn(
+            EventLoop::new(
+                network,
+                command_rx,
+                event_tx,
+                files_topic,
+                chat_topic,
+                address_topic,
+            )
+            .run(),
+        );
+        (Client { sender: command_tx }, event_rx)
+    }
+
+    /// Listen for incoming connections on the given address.
+    pub async fn start_listening(&mut self, addr: Multiaddr) {
+        self.sender
+            .send(Command::StartListening { addr })
+            .await
+            .unwrap();
+    }
+
+    /// Dial the given peer at the given address.
+    pub async fn dial(&mut self, peer_id: PeerId) -> Result<(), String> {
+        let (sender, receiver) = oneshot::channel();
+        self.sender
+            .send(Command::Dial { peer_id, sender })
+            .await
+            .unwrap();
+        receiver.await.unwrap()
+    }
+
+    /// Advertise the local node as the provider of the given file on the DHT.
+    pub async fn start_providing(&mut self, file_name: String) -> Result<(), String> {
+        let (sender, receiver) = oneshot::channel();
+        self.sender
+            .send(Command::Provide { file_name, sender })
+            .await
+            .unwrap();
+        receiver.await.unwrap()
+    }
+
+    /// Find the providers for the given file on the DHT.
+    pub async fn send_message(&mut self, message: String) -> Result<(), String> {
+        let (sender, receiver) = oneshot::channel();
+        self.sender
+            .send(Command::Message { message, sender })
+            .await
+            .unwrap();
+        receiver.await.unwrap()
+    }
+
+    /// Request the content of the given file from the given peer.
+    pub async fn request_file(&mut self, file_name: String) -> Result<Vec<u8>, String> {
+        let (sender, receiver) = oneshot::channel();
+        self.sender
+            .send(Command::Get { file_name, sender })
+            .await
+            .unwrap();
+        receiver.await.unwrap()
+    }
+}
+
 #[derive(NetworkBehaviour)]
-struct Behaviour {
+pub struct Behaviour {
     identify: identify::Behaviour,
     gossipsub: gossipsub::Gossipsub,
     relay: relay::v2::client::Client,
